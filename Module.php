aae507570b9bf8d856c72843cee93df596975089
Bas Kamer <bas@bushbaby.nl>
2012-11-01 15:14:05 +0100

    configured asset manager

--- Module.php
+++ Module.php
@@ -4,19 +4,8 @@ namespace BsbFontAwesome;
 
 class Module
 {
-//    public function getConfig()
-//    {
-//        return include __DIR__ . '/config/module.config.php';
-//    }
-
-    public function getAutoloaderConfig()
+    public function getConfig()
     {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
+        return include __DIR__ . '/config/module.config.php';
     }
 }
---
+++ config/module.config.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+    'asset_manager' => array(
+        'resolver_configs' => array(
+            'map' => array(
+                'css/font-awesome-ie7.css' => './vendor/fortawesome/fontawesome/css/font-awesome-ie7.css',
+                'css/font-awesome.css' => './vendor/fortawesome/fontawesome/css/font-awesome.css',
+                'font/fontawesome-webfont.eot' => './vendor/fortawesome/fontawesome/font/fontawesome-webfont.eot',
+                'font/fontawesome-webfont.svg' =>  './vendor/fortawesome/fontawesome/font/fontawesome-webfont.svg',
+                'font/fontawesome-webfont.ttf' => './vendor/fortawesome/fontawesome/font/fontawesome-webfont.ttf',
+                'font/fontawesome-webfont.woff' => './vendor/fortawesome/fontawesome/font/fontawesome-webfont.woff',
+            ),
+        ),
+    ),
+);
